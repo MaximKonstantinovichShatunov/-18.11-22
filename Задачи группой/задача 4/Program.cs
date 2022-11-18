0de10271205ec18d9983c5be62a36c84e67aceb8
maxim <shatunov-87@mail.ru>
2022-11-18 23:01:22 +0300

    push

---
+++ Задачи группой/задача 4/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine("Enter Number");
+    int a = int.Parse(Console.ReadLine()) % 10;
+    Console.WriteLine(a);
+  }
+}
+
